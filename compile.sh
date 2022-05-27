dfbdb269066bf2c5a8cf69c58e52ed7d50d907e5
plump_albert <plumpalbert@gmail.com>
2022-05-27 15:12:11 +0300

    Updated compile.sh script for CI

--- compile.sh
+++ compile.sh
@@ -3,10 +3,19 @@
 cd /root/texmf
 git pull
 cd "$SRC_DIR"
-latexmk \
-	-pdfxe -ps- -dvi- \
-	-xelatex \
-	-view=none -pvc \
-	-auxdir="$AUX_DIR" \
-	-outdir="$BUILD_DIR" \
-	$FILENAME
+if [ -z "$CI" ]; then
+	latexmk \
+		-pdfxe -ps- -dvi- \
+		-xelatex \
+		-view=none -pvc \
+		-auxdir="$AUX_DIR" \
+		-outdir="$BUILD_DIR" \
+		$FILENAME
+else
+	latexmk \
+		-pdfxe -ps- -dvi- \
+		-xelatex \
+		-auxdir="$AUX_DIR" \
+		-outdir="$BUILD_DIR" \
+		$FILENAME
+fi
